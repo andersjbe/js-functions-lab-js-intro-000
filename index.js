5416e36ecec50e036e891ea38097cee0242a62a0
Ben Anderson <andersjbe@gmail.com>
2020-03-22 21:34:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyCustomHolidayTo(name, holiday) {
 }
 
 function holidayCountdown(holiday, days) {
-  return "It's "+days+" days until ${holiday}!"
+  return "It's "+days+" days until "+holiday+"!"
 }
